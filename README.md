61fa9d82d966eaba0fc122ce2dbd635821d1b3ef
relato <hslj@relato.com.br>
2019-09-14 08:57:50 -0300

    Initial project

---
+++ README.md
@@ -0,0 +1,50 @@
+# Flask Blog python3 demo app
+
+## Installing python3 just in case
+[Install Python3 on mac](https://wsvincent.com/install-python3-mac/)
+
+## Clone the repository 
+```
+cd ~ 
+git clone https://github.com/RELATO/flaskblog.git
+cd flaskblog
+```
+
+## Installing the dependencies
+```
+pip3 install -r requirements.txt
+```
+
+## Developing on macos and mysql client
+You need to run the command below to install mysqlclient
+```
+env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip3 install mysqlclient
+```
+
+and after run
+```
+pip3 install configparser
+pip3 install mysql-python
+``` 
+
+## Generating the SECRET_KEY
+```
+python3
+>>> import secrets
+>>> print("Generate a secure hexadecimal token", secrets.token_hex(32))
+``` 
+You should see a message like the this
+```
+Generate a secure hexadecimal token 7865b88579dd1f82b36ec479b9b269c63c59a0a166f3f293c9ddb93b1f08ccc6
+```
+
+## Running the app
+Replace the statement below with your [dbuser] and [userpassword] before executing it
+```
+export SQLALCHEMY_DATABASE_URI='mysql+mysqldb://[dbuser]:[userpassword]@localhost/flaskblog'
+
+export SECRET_KEY=7865b88579dd1f82b36ec479b9b269c63c59a0a166f3f293c9ddb93b1f08ccc6
+
+
+python3 run.py
+```
---
+++ dbcreate.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Quality19*@localhost/flaskblog'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+admin = User('test', 'test@example.com', 'test123')
+
+db.create_all() # In case user table doesn't exists already. Else remove it.    
+
+db.session.add(admin)
+
+db.session.commit() # This is needed to write the changes to database
+
+User.query.all()
+
+User.query.filter_by(username='test').first()
